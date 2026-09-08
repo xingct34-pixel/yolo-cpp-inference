94a41eb7929604f9e0d45ac75abb5c28590b68e3
xingct34-pixel <xingct34@gmail.com>
2026-09-08 20:24:19 +0800

    Refactor comments for better code understanding
    Updated comments for clarity and improved code readability.

--- main.cpp
+++ main.cpp
@@ -9,19 +9,19 @@ using namespace cv;
 int main() {
     // 读取类别名称
     vector<string> class_names;                                   //相比于string names[80];，vector可以动态
-    ifstream f("/home/xct/cpp_projects/coco.txt");
+    ifstream f("/home/xct/cpp_projects/coco.txt");             //input‑file‑stream，文件读入流，专门用来从磁盘文件读取数据（只读，不写）
     string line;                                           //读一个文件或者用户输入,内容是一行一行的文字。这行代码就是提前准备一个"空盒子"(变量 line),专门用来临时装下每一次读到的一行内容。
     while (getline(f, line)) {
         class_names.push_back(line);
     }
-    f.close();
+    f.close();                                                   //// 关闭文件流，释放文件资源；ifstream对象析构时也会自动关闭，手动关闭更规范
     cout << "加载类别数量：" << class_names.size() << endl;
 
     // 加载模型
-    Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "yolo");
-    Ort::SessionOptions session_options;
-    const char* model_path = "/home/xct/cpp_projects/yolo11n.onnx";
-    Ort::Session session(env, model_path, session_options);
+    Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "yolo");                      //ONNX Runtime运行环境，先有环境才能跑模型 
+    Ort::SessionOptions session_options;                                  //配置选项，选择cpu或者gpu，模型cpu
+    const char* model_path = "/home/xct/cpp_projects/yolo11n.onnx";          //模型路径
+    Ort::Session session(env, model_path, session_options);              //创建推理‘会话’session
     cout << "模型加载成功" << endl;
 
     // 打开视频
@@ -32,8 +32,8 @@ int main() {
     }
     cout << "视频打开成功" << endl;
 
-    Mat img;                           //Mat 是 OpenCV 库里的一个类,全称是 Matrix(矩阵)，因为图像在计算机里本质上就是一个像素矩阵,所以叫 Mat。声明一个空的 Mat 对象,叫 img,现在还没有任何图像数据,只是先准备好这个"盘子",等下用来装每一帧画面。
-    int frame_count = 0;                //声明一个整数变量,用来给帧计数,从 0 开始(应该是后面循环里会用它记录处理了第几帧)。
+    Mat img;                           //Mat 是 OpenCV 库里的一个类,Matrix(矩阵)，图像在计算机里是像素矩阵。声明一个空的 Mat 对象,叫 img
+    int frame_count = 0;                //声明一个整数变量,用来给帧计数,从 0 开始。
 
     while (true) {
         // 读取一帧
@@ -48,14 +48,18 @@ int main() {
         blob.convertTo(blob, CV_32F, 1.0 / 255.0);        //把图像数据类型转换成 CV_32F(32位浮点数),同时每个像素值乘以 1.0/255.0，原始图像每个像素是 0~255 的整数(uchar)，这里把它归一化到 0~1 的浮点数范围,这是神经网络输入的常见要求
         cvtColor(blob, blob, COLOR_BGR2RGB); //把颜色通道顺序从 BGR 转成 RGB。，OpenCV 读图默认是 BGR 顺序，大多数深度学习框架(PyTorch/TensorFlow 训练出来的模型)习惯用 RGB 顺序,顺序对不上模型会认错颜色,精度大幅下降
 
-        Mat channels[3];
-        split(blob, channels);
-        vector<float> input_data;
+        Mat channels[3];                         //三个通道分别对应RGB
+        split(blob, channels);                    
+        vector<float> input_data;                           //浮点型容器，动态存储输入数据
         for (int c = 0; c < 3; c++) {
             input_data.insert(input_data.end(),
-                (float*)channels[c].data,
-                (float*)channels[c].data + 640 * 640);
+                (float*)channels[c].data,                      //起始指针
+                (float*)channels[c].data + 640 * 640);          //结尾指针，单通道总像素 = 409600 个 float 元素，指针向后偏移 409600 个 float 元素位置，得到这块 float 数组末尾的下一个位置
         }
+//为什么这么做？OpenCV存图片的方式（HWC）：像素1的R、G、B → 像素2的R、G、B → 像素3的R、G、B...           ONNX模型要求的方式（CHW）：所有像素的R → 所有像素的G → 所有像素的B
+
+
+
 
         array<int64_t, 4> input_shape{1, 3, 640, 640};  //NCHW 格式:N(batch)、C(channel)、H(height)、W(width)1 → Batch(批次数),表示一次只送 1 张图进去推理，3 → Channel(通道数),对应 R、G、B 三个通道，640 → Height(高度)，640 → Width(宽度)
         Ort::MemoryInfo memory_info = Ort::MemoryInfo::CreateCpu(
